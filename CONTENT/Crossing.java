2a730eac9457cb6f19297a955f5c88279c5bb40c
Max Hipp <34799688+maxxmodding@users.noreply.github.com>
2020-05-15 17:54:34 +0200

    Funktionen in MapGraph und Crossign hinzugefügt

--- CONTENT/Crossing.java
+++ CONTENT/Crossing.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 /**
  * Crossings sind die Knoten des Graphen
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @Max Hipp
+ * @15.05.2020
  */
 public class Crossing{
     private int id;
@@ -20,5 +20,8 @@ public class Crossing{
     }
     public void addStreet(Crossing tocrossing){
         streets.add(new Street(tocrossing));
+    }
+    public void removeStreet(){
+        
     }
 }
--- CONTENT/MapGraph.java
+++ CONTENT/MapGraph.java
@@ -2,22 +2,70 @@ package CONTENT;
 import java.util.ArrayList;
 
 /**
- * Aufbau des Straßensystems in Form eines Graphen (Knoten: Crossing; Kante: Street)
+ * Heimat der Adjazenzliste des Mapgraphen
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @Max Hipp
+ * @15.05.2020
  */
 public class MapGraph{
     private ArrayList<Crossing> crossings = new ArrayList<Crossing>();
-    
+
     public MapGraph(){
-        
+
     }
-    
+
+    /**
+     * fügt eine neue Crosing hinzu
+     */
     public void addCrossing(int id){
         crossings.add(crossings.size(), new Crossing(id));
     }
+
+    /**
+     * fügt eine Street (Kante) zwischen zwei Crossings (Knoten) hinzu
+     */
     public void addStreet(int fromid, int toid){
+        Crossing fromCr = null;
+        Crossing toCr = null;
+
+        for(int i = 0; i < crossings.size(); i++){
+            if(fromid == (crossings.get(i)).returnID()){
+                fromCr = crossings.get(i);
+                System.out.println("Success!");
+                for(int j = 0; j < crossings.size(); j++){
+                    if(toid == (crossings.get(j)).returnID()){
+                        toCr = crossings.get(j);
+                        System.out.println("Success!");
+                        break;
+                    }
+                    else if(j == crossings.size()){
+                        toCr = null;
+                        System.out.println("Crossing 2 not found");
+                    }
+                }
+                break;
+            }
+            else if(i == crossings.size()){
+                fromCr = null;
+                System.out.println("Crossing 1 not found");
+            }
+        }
+
+        if (toCr != null && fromCr != null){
+            fromCr.addStreet(toCr);
+            toCr.addStreet(fromCr);
+            System.out.println("Success!");
+        }
+        else{
+            System.out.println("At least 1 crossing could not be found!");
+        }
+    }
+    
+    public void removeCrossing(){
+        
+    }
+    
+    public void removeStreet(){
         
     }
 }
